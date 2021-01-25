9e6b246f4d0529602122b13bc7ca62df6690cd14
Yannick <yannick.aiguier@gmail.com>
2021-01-25 15:35:04 +0100

    v1.4 le home controller est prêt

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
 $routes = [
-    'defaut' => 'config/database.php',
+    '' => 'app/controllers/homeController.php',
+    'accueil' => 'app/controllers/homeController.php',
+    'index.php' => 'app/controllers/homeController.php',
     '404' => 'pages/404' // a changer, definir une page 404 et son emplacement
 ];
 if (isset($_GET['action'])) { // action a réaliser
-    $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+    $page = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
     if (!array_key_exists($page, $routes)) {
         $page = '404';
     }
 } else { // page par défaut
-    $page = 'defaut'; // a modifier ulterieurement
+    $page = 'accueil';
 }
 require 'config/database.php';
+require $routes[$page];
