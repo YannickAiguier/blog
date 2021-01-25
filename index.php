2b288ba2dba5e5931f1f4a139a1833e8c02e60c3
Yannick <yannick.aiguier@gmail.com>
2021-01-25 11:29:41 +0100

    v1.2 Structures des répertoires créée, front controller prêt

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$routes = [
+    'defaut' => 'index.html',
+    '404' => 'pages/404' // a changer, definir une page 404 et son emplacement
+];
+if (isset($_GET['action'])) { // action a réaliser
+    $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+    if (!array_key_exists($page, $routes)) {
+        $page = '404';
+    }
+} else { // page par défaut
+    $page = 'defaut'; // a modifier ulterieurement
+}
+require $routes[$page];
